83f88164d7b2f96206e6792cf4db63d3c802ba02
Margaret Berry and Mark Helt <margaret@>
2017-10-23 14:31:09 -0700

    add mock heroes and hero service components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -72,19 +73,4 @@ export class AppComponent {
   }
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Batwoman' },
-  { id: 12, name: 'Catman' },
-  { id: 13, name: 'Spiderwoman' },
-  { id: 14, name: 'Wonderman' },
-  { id: 15, name: 'IronWoman' },
-  { id: 16, name: 'Mrs. Fantastic' },
-  { id: 17, name: 'Mrs. Terrific' },
-  { id: 18, name: 'Luca Cage' },
-  { id: 19, name: 'Juan Grey' },
-  { id: 20, name: 'Black Widower' },
-  { id: 21, name: 'She Woman' },
-  { id: 22, name: 'Black Widower' },
-  { id: 23, name: 'Aqua Woman' },
-  { id: 24, name: 'Hell girl' }
-];
+heroes: Hero[];
