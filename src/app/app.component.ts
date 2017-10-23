dbb562cace7142f073d4e0ac924fe9da23740204
Margaret Berry and Mark Helt <margaret@>
2017-10-23 11:48:19 -0700

    add styling to app.compontent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,96 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name">
     </div>
-  `
+  `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title: string = 'Tour of Heroes';
   hero: Hero = {
   id: 1,
   name: 'Windstorm'
   };
+  heroes = HEROES;
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Batwoman' },
+  { id: 12, name: 'Catman' },
+  { id: 13, name: 'Spiderwoman' },
+  { id: 14, name: 'Wonderman' },
+  { id: 15, name: 'IronWoman' },
+  { id: 16, name: 'Mrs. Fantastic' },
+  { id: 17, name: 'Mrs. Terrific' },
+  { id: 18, name: 'Luca Cage' },
+  { id: 19, name: 'Juan Grey' },
+  { id: 20, name: 'Black Widower' },
+  { id: 21, name: 'She Woman' },
+  { id: 22, name: 'Black Widower' },
+  { id: 23, name: 'Aqua Woman' },
+  { id: 24, name: 'Hell girl' }
+];
